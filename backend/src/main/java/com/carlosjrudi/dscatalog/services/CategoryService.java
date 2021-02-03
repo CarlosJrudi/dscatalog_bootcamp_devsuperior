ef406498ad678dc180a5501d37c4369ed328b868
Carlos Jr <carlosjrudi@hotmail.com>
2021-02-03 06:13:45 -0300

    Find category by id + exception handler

--- backend/src/main/java/com/carlosjrudi/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/carlosjrudi/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.carlosjrudi.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.carlosjrudi.dscatalog.DTO.CategoryDTO;
 import com.carlosjrudi.dscatalog.entities.Category;
 import com.carlosjrudi.dscatalog.repositories.CategoryRepository;
+import com.carlosjrudi.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,5 +24,12 @@ public class CategoryService {
 		List <Category> list = repository.findAll();
 		
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional (readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found / Objeto não encontrado."));	
+		return new CategoryDTO(entity);
 	}
 }
